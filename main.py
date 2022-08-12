d0a9cab6a6fd9253ae1acf9a584fe0635ee72eb6
unknown <lin900123@gmail.com>
2022-08-13 04:14:56 +0800

    add try except to debug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # from pprint import pprint
+from tabnanny import check
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -6,20 +7,20 @@ from config import username,password,headers,url,ajax_url,p_url,path
 from datetime import datetime
 from openpyxl import Workbook
 import compare
-import os
+import os, sys
 
 def create_folder(path):
     if not os.path.isdir(path):
         os.makedirs(path)
 
 def ask_excel(ask_option):
-    flag =''
+    flag = []
     yes_list = ['y','Y','yes']
-    no_list = ['n','N','no']
+    no_list = ['n','N','no','']
     while flag not in yes_list and flag not in no_list:
-        flag = input(f'Do u want {ask_option} ? y/n: ')
+        flag = input(f'Do u want {ask_option}? y/n [n]: ')
         if(flag not in yes_list and flag not in no_list):
-            print('plz enter y or n !!!')
+            print('plz enter y or n or ENTER!!!')
     # print(f'flag={flag}')
     if(flag in yes_list):
         return 1
@@ -60,7 +61,13 @@ def main():
         'queryParams': {},
         'optIntoOneTap': 'false'
     }
-    uid = str(input('Enter id: '))
+    print(f'If target is private account, you have to follow it first!!!')
+    while True:
+        uid = str(input('Enter id: '))
+        if (uid == ''):
+            print(f'Do not leave blank!!!')
+        else:
+            break
     while True:
         option = str(input('following[1]/followers[2]: '))
         if(option == '1'):
@@ -71,7 +78,11 @@ def main():
             break
         else:
             print(f'enter 1 or 2 or following or followers!!!')
-    fcount = str(input('Enter max num of following/followers: '))
+    try:
+        fcount = int(input('Enter max num of following/followers [2000]: '))
+    except ValueError:
+        fcount = 2000
+
     opt_title = {
         'following': 'fwi',
         'followers': 'fwr',}
@@ -88,10 +99,22 @@ def main():
         session.post(ajax_url, data=payload, headers=headers, cookies=cookies)          #用現有cookie和csrf token 去取得登入的session
         # print(req2.text)
 
-        fsi=session.get(p_url+uid,cookies=cookies,headers=headers)      
-        friendid = str(re.findall(r"id\":\"(.*?)\"",fsi.text)[1])   
-        print("userid:",friendid)
+        fsi=session.get(p_url+uid,cookies=cookies,headers=headers)    
         # print(fsi.text)
+        
+        try:
+            # print(str(re.findall(r"id\":\"(.*?)\"",fsi.text)))
+            friendid = str(re.findall(r"id\":\"(.*?)\"",fsi.text)[1])
+            checkid = str(re.findall(r"id\":\"(.*?)\"",fsi.text)[-1])
+            if(friendid == '236' or friendid == None or checkid == '236'):
+                raise Exception
+            print(f"userid:{friendid}")
+        except:
+            print(f'error while checking userid')
+            print(f'1.plz check the target username(no @)!!!')
+            print(f'2.Make sure u set the right USERNAME and PASSWORD in *config.py* file!!!')
+            print(f'3.your account might block by instagram server, plz try again later or change your ip!!')
+            sys.exit()
 
         # url的後輟 可以像翻頁一樣去增加再爬取 或是直接爆max來爬取
         params = {
@@ -99,17 +122,35 @@ def main():
         'max_id': ''}
         response = session.get(f'https://i.instagram.com/api/v1/friendships/{friendid}/{option}/', params=params, cookies=cookies, headers=headers)
         # print(response.text)
-        root_json = response.json()
+        try:
+            root_json = response.json()
+        except requests.exceptions.JSONDecodeError as jsonError:
+            print(f'Error when processing json file: {jsonError}')
+            print(f'1.Make sure u set the right USERNAME and PASSWORD in *config.py* file!!!')
+            print(f'2.your account might block by instagram server, plz try again later or change your ip!!')
+            sys.exit()
+            
     if(ask == 1):
-        do_excel(uid,date,opt_title,option,root_json,path)
+        try:
+            do_excel(uid,date,opt_title,option,root_json,path)
+        except IOError as error:
+            print(f'Error when generate Excel file:{error}')
     # pprint(response.text)
     do_txt(uid,date,opt_title,option,root_json,path)
     ask2 = ask_excel('compare with old file')
     if(ask2 == 1):
-        f1 = path+input(f'Enter first filename(older file): ')+'.txt'
-        f2 = path+f'{uid}{date}{opt_title[option]}'+'.txt'
-        compare.compare_file(f1, f2)
+        try:
+            f1 = path+input(f'Enter first filename(older file): ')+'.txt'
+            f2 = path+f'{uid}{date}{opt_title[option]}'+'.txt'
+            compare.compare_file(f1, f2)
+        except IOError as error:
+            print(f'Error when generate compared.txt file:{error}')
+            print(f'Make sure u have the file existed and enter the right filename(without .txt)!!!')
+            sys.exit()
 
 if __name__ == '__main__':
     create_folder(path)
+    if username == 'USERNAME or EMAIL':
+        print(f'plz go config.py to set your USERNAME and PASSWORD')
+        os._exit(0)
     main()
